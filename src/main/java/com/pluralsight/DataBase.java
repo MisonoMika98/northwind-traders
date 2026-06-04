4407d40aeedb510355e0c2b63c34cb5eccc7d277
ariel raymundo <ariraymundo98@gmail.com>
2026-06-04 13:41:12 -0700

    added code, can be ran through main

--- src/main/java/com/pluralsight/DataBase.java
+++ src/main/java/com/pluralsight/DataBase.java
@@ -2,15 +2,46 @@ package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-public class Main
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataBase
 {
-    static void main()
+    public static void databaseTest()
     {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
         dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setPassword("yearup26");
 
+        String sql = "SELECT ProductName FROM products";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery())
+        {
+
+            while (rs.next())
+            {
+
+                String name = rs.getString("ProductName");
+                double price = rs.getDouble("UnitPrice");
+                System.out.printf("%s: $%.2f%n", name, price);
+
+
+
+            }
+
+        }
+
+        catch (SQLException e)
+        {
+
+            e.printStackTrace();
+
+        }
     }
+
+
 }
